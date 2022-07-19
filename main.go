b3cebff967519d9948d485ebe3dc6470f0876e08
serditizol <serditizol@gmail.com>
2022-07-19 16:22:47 +0700

    Правки в header

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"os"
 
 	"github.com/gorilla/mux"
 )
@@ -13,16 +14,44 @@ type catalog struct {
 	name, image, text, price string
 }
 
-func index(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles("templates/index.html", "shablon/header.html", "shablon/footer.html")
-	if err != nil {
-		panic(err)
+func isExist(name string) bool {
+	if _, err := os.Stat(name); err != nil {
+		if os.IsNotExist(err) {
+			return false
+		}
 	}
-	t.Execute(w, "index")
+	return true
+}
+
+// Папка, которая будет доступна на web сервере
+func includeStaticFolder(folder string) {
+	http.Handle(folder, http.StripPrefix(folder, http.FileServer(http.Dir("."+folder))))
+}
+func includeStatic() {
+	includeStaticFolder("/css/")
+	includeStaticFolder("/images/")
+	includeStaticFolder("/fonts/")
+}
+
+func include_str(url_str string, w http.ResponseWriter, r *http.Request) {
+	if isExist(url_str) {
+		t, err := template.ParseFiles(url_str, "shablon/header.html", "shablon/footer.html")
+		if err != nil {
+			panic(err)
+		}
+		t.Execute(w, include_str)
+	}
+}
+
+// Подключение страниц
+func index(w http.ResponseWriter, r *http.Request) {
+	include_str("templates/index.html", w, r)
 }
 
 func furl() {
+	includeStatic()
 	r := mux.NewRouter()
+	// r.HandleFunc("/", index).Methods("GET")
 	r.HandleFunc("/", index).Methods("GET")
 	http.Handle("/", r)
 
